e20be654446623152f67c7cf2381c648f88fa33d
AleksandraMN <al_mn@mail.ru>
2024-08-30 22:01:09 +1000

    Lesson02

---
+++ src/components/Header/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './Logo.module.css';
+import logo from './img/logo.svg';
+
+export const Logo = () => {
+  return (
+    <a className={style.Link} href='/'>
+      <img className={style.Logo} src={logo} alt="Логотип Blogget" />
+    </a>
+  );
+};
---
+++ src/components/Main/Layout/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './Layout.module.css';
+
+export const Layout = (props) => {
+  return (
+    <div className={style.container}>
+      {props.children}
+    </div>
+  );
+};
