e59d5c86837173eccd9f0ad5ff602ac372384f53
AleksandraMN <al_mn@mail.ru>
2024-10-15 20:02:36 +1000

    Lesson14

--- src/api/const.js
+++ src/api/const.js
@@ -3,5 +3,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'LD-ZEwbdC3vw1F8K0wmXpA';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'http://blogget-three-eta.vercel.app/auth';
 export const SCOPE = 'identity submit read';
